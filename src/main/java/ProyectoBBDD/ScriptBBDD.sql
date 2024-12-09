2b29dd7d5d00c5f596605d9b396da00ec5c86744
alumno <alumno@host.docker.internal>
2024-12-09 12:40:01 +0100

    practica nueva

--- src/main/java/ProyectoBBDD/ScriptBBDD.sql
+++ src/main/java/ProyectoBBDD/ScriptBBDD.sql
@@ -1,13 +1,24 @@
 create database tienda;
 use tienda;
 create table clientes(
-codigo_cliente int PRIMARY KEY NOT NULL
+codigo_cliente VARCHAR(6) PRIMARY KEY NOT NULL,
+nif VARCHAR(9) UNIQUE NOT NULL,
+apellidos VARCHAR(35) NOT NULL,
+nombre VARCHAR(15) NOT NULL,
+domicilio VARCHAR(40) NOT NULL,
+codigoPostal VARCHAR(5) NOT NULL,
+localidad VARCHAR(20) NOT NULL,
+telefono VARCHAR(9) NOT NULL,
+movil VARCHAR(9) NOT NULL,
+fax VARCHAR(9) NOT NULL,
+mail VARCHAR(20) NOT NULL,
+TotalVentas FLOAT
 );
 
 create table proveedores(
-codigo_proveedor int PRIMARY KEY NOT NULL
+codigo_proveedor INTEGER PRIMARY KEY NOT NULL
 );
 
 create table articulos(
-codigo_articulo int PRIMARY KEY NOT NULL
+codigo_articulo INTEGER PRIMARY KEY NOT NULL
 );
